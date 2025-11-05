ab92ba6e30bbe1b421126b74a7812b82e383dda7
SatrioHalim <satriohalim21@gmail.com>
2025-11-05 22:04:30 +0700

    First init

---
+++ UTS_AlgoProg_2025.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void menu(){
+	int choice = 0;
+	do{
+		system("cls");
+		printf("List of case solved\n");
+		printf("====================\n");
+		printf("1. Let's Play roblox'")
+	}while(choice != 6);
+}
+
+void intro(){
+	printf("++++++++++++++++++++++++\n");
+	printf("Pre-UTS AlgoProg 2025   \n");
+	printf("========================\n");
+	printf("         By          \n");
+	printf("    Silver Bullet    \n");
+	printf("\n\nPress enter to continue...");
+	getchar();
+}
+
+
+int main(){
+	intro();
+		
+	
+	
+	return 0;
+}
